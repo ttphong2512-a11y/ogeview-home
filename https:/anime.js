ab2b14611405d918d914b580a34e5c32b99c2483
ttphong2512-a11y <ttphong2512@gmail.com>
2026-07-30 22:17:40 +0700

    Create anime.js

---
+++ https:/anime.js
@@ -0,0 +1,17 @@
+const animeList = [
+  {
+    title: "Demon Slayer",
+    poster: "https://YOUR-POSTER.jpg",
+    links: {
+      vi: "https://www.ogeview.online/2026/07/demon-slayer.html",
+      en: "https://www.ogeview.online/2026/07/demon-slayer-en.html"
+    }
+  },
+  {
+    title: "One Piece",
+    poster: "https://YOUR-POSTER2.jpg",
+    links: {
+      vi: "https://www.ogeview.online/2026/07/one-piece.html"
+    }
+  }
+];
