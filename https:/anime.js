cc76957dc7a7388d77a76157dd701441e135f123
ttphong2512-a11y <ttphong2512@gmail.com>
2026-07-30 22:37:55 +0700

    Update anime.js

--- https:/anime.js
+++ https:/anime.js
@@ -1,7 +1,7 @@
 const animeList = [
   {
     title: "Demon Slayer",
-    poster: "https://YOUR-POSTER.jpg",
+    poster: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiP-HOKES_d664x7HqFv11XruXDKSmlFTr9woqmcQUJ0vcPmMYl_Tiwmq5IL5Piq4G3v7bg9Pn8h_vxsGhz1EvLSwb83zitZlawGYXrOnKkqw6TEY2ZThddk7IyE6UZCCM6L-Q_T9LZPdoQ2xtJvRvS2RyDvzo2OVGNcFRTakUmVQHktIYHI7U7D0aqueg/s547/1000024081.jpg",
     links: {
       vi: "https://www.ogeview.online/2026/07/demon-slayer.html",
       en: "https://www.ogeview.online/2026/07/demon-slayer-en.html"
